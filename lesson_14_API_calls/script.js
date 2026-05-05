1d1a5d37afd9aff74e46ea5980d772fd88b8aa91
Tornike Kapanadze <tornikek3@gmail.com>
2026-05-05 12:12:58 +0400

    Add local json server to check post and delete methods

--- lesson_14_API_calls/script.js
+++ lesson_14_API_calls/script.js
@@ -1,74 +1,112 @@
-async function getPosts() {
-  try {
-    const respons = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "GET",
-    });
+// async function getPosts() {
+//   try {
+//     const respons = await fetch("http://localhost:3000/posts", {
+//       method: "GET",
+//     });
 
-    if (!respons.ok) {
-      throw new Error("Can't fetch Posts");
-    }
+//     if (!respons.ok) {
+//       throw new Error("Can't fetch Posts");
+//     }
 
-    const data = await respons.json();
+//     const data = await respons.json();
 
-    return data;
-  } catch (err) {
-    console.log(err.message);
-    return [];
-  }
-}
+//     return data;
+//   } catch (err) {
+//     console.log(err.message);
+//     return [];
+//   }
+// }
 
-const newPost = {
-  userId: 200,
-  title: "new post",
-  body: "this is a new post",
-};
+// const newPost = {
+//   userId: 200,
+//   title: "new post",
+//   body: "this is a new post",
+// };
 
-async function createPost() {
-  try {
-    const respons = await fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(newPost),
-    });
+// async function createPost() {
+//   try {
+//     const respons = await fetch("", {
+//       method: "POST",
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//       body: JSON.stringify(newPost),
+//     });
 
-    if (!respons.ok) {
-      throw new Error("Cant craete new post");
-    }
+//     if (!respons.ok) {
+//       throw new Error("Cant craete new post");
+//     }
 
-    let newData = await respons.json();
+//     let newData = await respons.json();
 
-    console.log(newData);
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     console.log(newData);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-createPost();
+// // createPost();
 
-async function displayPosts() {
-  const posts = await getPosts();
+// async function displayPosts() {
+//   const posts = await getPosts();
 
-  const finalString = posts
-    .map((post) => {
-      return `  
-          
-        <div class="post">
-        
-        <h2> Post Id is: ${post.id} </h2>
+//   const finalString = posts
+//     .map((post) => {
+//       return `
 
-        <h3> Post title is: ${post.title} </h3>
+//         <div class="post">
 
-        <p> Post body is: ${post.body} </p>
+//         <h2> Post Id is: ${post.id} </h2>
 
-        </div>
-        
-        `;
-    })
-    .join("");
+//         <h3> Post title is: ${post.title} </h3>
 
-  document.querySelector(".container").innerHTML = finalString;
-}
+//         <p> Post body is: ${post.body} </p>
+
+//         </div>
+
+//         `;
+//     })
+//     .join("");
+
+//   document.querySelector(".container").innerHTML = finalString;
+// }
 
 // displayPosts();
+
+async function getPosts() {
+  let respons = await fetch("http://localhost:3000/posts", { method: "GET" });
+
+  let data = await respons.json();
+
+  console.log(data);
+}
+
+getPosts();
+
+async function createPost(post) {
+  const respons = await fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(post),
+  });
+}
+
+// setInterval(() => {
+//   const newPost = {
+//     userId: "11",
+//     title: "title eleven",
+//     body: "body eleven",
+//   };
+
+//   createPost(newPost);
+// }, 10000);
+
+async function deletePost(id) {
+  const response = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: "DELETE",
+  });
+}
+
+// setInterval(() => {
+//   deletePost("5");
+// }, 10000);
