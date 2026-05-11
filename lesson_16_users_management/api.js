5d4e0a3c0399032def7266805657f392d909a7a0
Tornike Kapanadze <tornikek3@gmail.com>
2026-05-11 10:59:16 +0400

    Add lesson 16

---
+++ lesson_16_users_management/api.js
@@ -0,0 +1,14 @@
+const URL = "http://localhost:3000/users";
+
+// create new user
+export async function createUser(user) {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(user),
+  });
+
+  const result = await response.json();
+
+  console.log(result);
+}
---
+++ lesson_16_users_management/script.js
@@ -0,0 +1,20 @@
+import { createUser } from "./api.js";
+
+document.querySelector("#userForm").addEventListener("submit", (e) => {
+  const name = document.querySelector("#name").value;
+  const surname = document.querySelector("#surname").value;
+  const email = document.querySelector("#email").value;
+  const userId = document.querySelector("#userId").value;
+
+  const newUser = {
+    id: crypto.randomUUID(),
+    name,
+    surname,
+    email,
+    userId,
+  };
+
+  createUser(newUser);
+
+  e.target.reset();
+});
